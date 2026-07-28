bd37328f25f109ed6dbc8bb1830a613fd2867956
ibrahem mamdouh <synapticadmin@gmail.com>
2026-07-28 11:50:20 +0300

    chore: move install-apks.bat to scripts/

--- install-apks.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=C:\Users\kayf\AppData\Local\Android\Sdk\platform-tools;%PATH%
-adb install -r "%USERPROFILE%\Desktop\SynapticGo-APKs\synaptic-go-rider-debug.apk"
-adb install -r "%USERPROFILE%\Desktop\SynapticGo-APKs\synaptic-go-captain-debug.apk"
-pause
