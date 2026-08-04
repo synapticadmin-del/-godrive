96ca2d3d2ddf151851f7d0a5621b4e04e3652e62
Tempo Rebrand Agent <developer-agent@synapticstudio.tech>
2026-08-04 13:50:19 +0300

    chore(rider): retitle the app to Tempo in app_localizations_en.dart
    Part of the GoDrive to Tempo rebrand.

--- apps/rider/lib/l10n/generated/app_localizations_en.dart
+++ apps/rider/lib/l10n/generated/app_localizations_en.dart
@@ -5,7 +5,7 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get appTitle => 'GoDrive';
+  String get appTitle => 'Tempo';
 
   @override
   String get appSlogan => 'Your road is always green';
